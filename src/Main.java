81540dd7481675a1e9f758fb212df97efa826233
Denis <denisbayraktar@outlook.com>
2024-01-01 20:31:58 +0300

    Kullanıcıdan üç kenar uzunluğu alınan üçgenin alanını hesaplayan program kodları eklendi.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,29 @@ public class Main {
     public static void main(String[] args) {
         int dikKenar1, dikKenar2;
         Scanner input1 = new Scanner(System.in);
-        System.out.print("Üçgenin 1. dik kenarını giriniz: ");
+        System.out.print("Üçgenin birinci dik kenarını giriniz: ");
         Scanner input2 = new Scanner(System.in);
         dikKenar1 = input1.nextInt();
-        System.out.print("Üçgenin 2. dik kenarını giriniz: ");
+        System.out.print("Üçgenin ikinci dik kenarını giriniz: ");
         dikKenar2 = input2.nextInt();
         double hipotenusHesaplama = Math.sqrt(Math.pow(dikKenar1,2) + Math.pow(dikKenar2,2));
         System.out.println("Hipotenüs: " + hipotenusHesaplama);
+
+        System.out.println("------------------------------------------------------------");
+
+        double a, b, c;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Üçgenin birinci kenarını giriniz: ");
+        a = input.nextDouble();
+        System.out.println("Üçgenin ikinci kenarını giriniz: ");
+        b = input.nextDouble();
+        System.out.println("Üçgenin üçüncü kenarını giriniz: ");
+        c = input.nextDouble();
+
+        double u = (a + b + c) / 2;
+        double alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+
+        System.out.println("Üçgenin alanı: " + alan);
     }
 }
